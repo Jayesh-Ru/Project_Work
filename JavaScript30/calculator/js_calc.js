69b63acbb3626fd67d5a6a7fd0224d652a0093bb
Jayesh-Ru <rockykhairnar2099@gmail.com>
2022-12-25 00:41:46 +0530

    added the javascript projects

---
+++ JavaScript30/calculator/js_calc.js
@@ -0,0 +1,16 @@
+// This function clear all the values
+function clears(){
+    document.getElementById("result").value = "";
+}
+ 
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
+ 
+// This function evaluates the expression and returns result
+function cal() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
